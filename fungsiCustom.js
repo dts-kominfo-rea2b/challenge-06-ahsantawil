02d59d60da12da403772fa5f30c19df3f267bfa8
ahsantawil <ahsan.tawil07@gmail.com>
2022-06-10 11:19:15 +0700

    Challenge 6 - Callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require('fs');
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,37 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = async (fnCallback) => {
+ let listFile = [file1, file2, file3];
+ let data = [];
+
+  const dataFile = (rawData) => {
+    if (rawData.message !== undefined) {
+      let processedData = rawData.message.split(" ");
+      return processedData[processedData.length - 1];
+    }
+  
+    if (rawData[0].message !== undefined) {
+      let processedData = rawData[0].message.split(" ");
+      return processedData[processedData.length - 1];
+    }
+  
+    if (rawData[0].data.message !== undefined) {
+      let processedData = rawData[0].data.message.split(" ");
+      return processedData[processedData.length - 1];
+    }
+  };
+
+  try {
+    for (const a of listFile) {
+      const result = await fs.promises.readFile(a, "utf-8");
+      data.push(dataFile(JSON.parse(result)));
+    }
+    fnCallback(null, data);
+  } catch (err) {
+    fnCallback(err, data);
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
